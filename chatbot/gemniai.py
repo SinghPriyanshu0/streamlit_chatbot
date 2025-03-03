03f81c86b7511f9893ba551c4cf8260d0a310fac
SinghPriyanshu0 <psingh@bytepx.com>
2025-03-03 13:13:35 +0530

    Update gemniai.py

--- chatbot/gemniai.py
+++ chatbot/gemniai.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 
 def chat_with_gemini(user_input):
     """Generates a response using Google Gemini AI."""
-    model = genai.GenerativeModel("gemini-pro")
+    model = genai.GenerativeModel("gemini-1.5-pro-latest")
     response = model.generate_content(user_input)
     return response.text
 
